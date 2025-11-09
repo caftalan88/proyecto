92615932944403edd9604639a35e3af315115feb
caftalan88 <catalanespinoza@gmail.com>
2025-11-08 23:40:25 -0300

    Update __init__.py

--- test_escaner/app/__init__.py
+++ test_escaner/app/__init__.py
@@ -11,10 +11,11 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
+
+    from .routes import main
     app.register_blueprint(main)
 
     with app.app_context():
         db.create_all()
 
     return app
-
