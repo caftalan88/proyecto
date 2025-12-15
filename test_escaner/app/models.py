29aeb3a5b2be90833e6b0326639a2a6a03d20a23
caftalan88 <catalanespinoza@gmail.com>
2025-12-15 13:56:50 -0300

    Add EstadoDispositivoLog model and update Dispositivo

--- test_escaner/app/models.py
+++ test_escaner/app/models.py
@@ -3,25 +3,46 @@ from . import db
 
 
 class Dispositivo(db.Model):
+    __tablename__ = "dispositivo"
+
     id = db.Column(db.Integer, primary_key=True)
     ip = db.Column(db.String(50), unique=True, nullable=False)
     mac = db.Column(db.String(50))
     nombre = db.Column(db.String(100))
+    tipo = db.Column(db.String(50), default="Desconocido")
     riesgo = db.Column(db.String(20), default="Desconocido")
     fecha_ultima_visto = db.Column(db.DateTime, default=datetime.utcnow)
+
+    estado = db.Column(db.String(20), default="activo")
 
     def __repr__(self):
         return f"<Dispositivo {self.ip} ({self.mac})>"
 
+class EstadoDispositivoLog(db.Model):
+    __tablename__ = "estado_dispositivo_log"
+
+    id = db.Column(db.Integer, primary_key=True)
+    dispositivo_id = db.Column(db.Integer, db.ForeignKey("dispositivo.id"), nullable=False)
+    estado_anterior = db.Column(db.String(20))
+    estado_nuevo = db.Column(db.String(20))
+    motivo = db.Column(db.String(255))
+    fecha_cambio = db.Column(db.DateTime, default=datetime.utcnow)
+
+    dispositivo = db.relationship("Dispositivo", backref=db.backref("historial_estados", lazy=True))
 
 class Escaneo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fecha = db.Column(db.DateTime, default=datetime.utcnow)
     total_dispositivos = db.Column(db.Integer, default=0)
     dispositivos_vulnerables = db.Column(db.Integer, default=0)
+    duracion_segundos = db.Column(db.Float, nullable=True)
 
     def __repr__(self):
-        return f"<Escaneo {self.fecha} - {self.total_dispositivos} dispositivos>"
+        return (
+            f"<Escaneo {self.fecha} - "
+            f"{self.total_dispositivos} dispositivos - "
+            f"{self.duracion_segundos or 0}s>"
+        )
 
 
 class EventoRed(db.Model):
